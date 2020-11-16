f4520846de6c6d1130d2a8fcb658c32ceb15b93d
Unkovic7 <stefan.unkovic2003@gmail.com>
2020-11-16 02:58:58 +0100

    Finished first login and registration;

--- assets/database/database_impl.js
+++ assets/database/database_impl.js
@@ -1,31 +1,41 @@
 require('dotenv').config();
-var mysql = require('mysql');
+const mariadb = require('mariadb/callback');
+/// <summary>connection configuration</summary>
+
 /// <summary>Connection arguments</summary>
 
-const con = mysql.createConnection({
+const connection = mariadb.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    db: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE
 });
+module.exports = connection;
 
 /// <summary>Establishing connection</summary>
 
-samp.on(EVENT_GAME_MODE_INIT, con.connect((err) => {
-    if(err) {
-        console.error(err.message);
-        samp.callNative('SendRconCommand', 's', 'exit');
-        return 1;
-    }
-    console.log('Yes, connection succes ');
+samp.on(EVENT_GAME_MODE_INIT, async () => {
+    connection.connect(err => {
+        if (err) {
+            console.error('Failed to connect database', err);
+        } else {
+            console.log('Success to connect database');
+        }
+    
+    })
     return 1;
-
-}))
+})
 
 /// <summary>Destroying connection</summary>
 
-samp.on(EVENT_GAME_MODE_EXIT, (() => {
-    con.destroy();
-    console.info('Connection destroyed');
+samp.on(EVENT_GAME_MODE_EXIT, async () => {
+    connection.end(err => {
+        if (err) {
+            console.error('Failed to end database', err);
+        } else {
+            console.log('Success to end database');
+        }
+    })
     return 1;
-}));
+})
+
